2d4a06d18efc69c7ddf4cea9270ac5f64f565bda
Dead Nate <nathan.yukio.stephens@gmail.com>
2021-02-15 20:09:22 -0800

    player object created and passed test

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -7,7 +7,7 @@ console.log(new Potion);
 
 const Player = require('../lib/Player');
 
-test('creates a player object', ()=> {
+test('creates a player object', () => {
     const player = new Player('Dave');
 
     expect(player.name).toBe('Dave');
@@ -17,4 +17,25 @@ test('creates a player object', ()=> {
     expect(player.inventory).toEqual(
         expect.arrayContaining([expect.any(Object)])
     );
+
+});
+
+
+test("gets player's stats as an object", () => {
+    const player = new Player('Dave');
+
+    expect(player.getStats()).toHaveProperty('potions');
+    expect(player.getStats()).toHaveProperty('health');
+    expect(player.getStats()).toHaveProperty('strength');
+    expect(player.getStats()).toHaveProperty('agility');
+});
+
+test('gets inventory from player or returns false', () => {
+    const player = new Player('Dave');
+
+    expect(player.getInventory()).toEqual(expect.any(Array));
+
+    player.inventory = [];
+
+    expect(player.getInventory()).toEqual(false);
 });
